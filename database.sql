01416f8425fb91f44fb2b9176d8a0db7faaae14d
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-09-05 13:10:34 -0500

    troubleshooting interpretation route

--- database.sql
+++ database.sql
@@ -3,8 +3,20 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
+
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+  "id" SERIAL PRIMARY KEY,
+  "username" VARCHAR(80) UNIQUE NOT NULL,
+  "password" VARCHAR(1000) NOT NULL,
+  "profile_pic_url" TEXT
+);
+
+CREATE TABLE "dream" (
+  "id" SERIAL PRIMARY KEY,
+  "user_id" INTEGER NOT NULL,
+  "dream_description" TEXT NOT NULL,
+  "dream_interpretation" TEXT,
+  "date" DATE NOT NULL,
+  "dream_image_url" TEXT,
+  FOREIGN KEY ("user_id") REFERENCES "user" ("id")
 );
