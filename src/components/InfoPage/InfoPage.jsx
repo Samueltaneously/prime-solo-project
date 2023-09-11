acf5251c814a99ca65fb1de45072f2058cfd2e1c
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-09-11 14:03:42 -0500

    info page cards working

--- src/components/InfoPage/InfoPage.jsx
+++ src/components/InfoPage/InfoPage.jsx
@@ -1,32 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom';
-import { useState, useEffect } from 'react';
-import LogOutButton from '../LogOutButton/LogOutButton';
-
+import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
-import Box from '@mui/material/Box';
-// import { makeStyles } from '@mui/material/styles';
-
+import LogOutButton from '../LogOutButton/LogOutButton';
 
 function InfoPage() {
-
-  // const useStyles = makeStyles((theme) => ({
-  //   right: {
-  //     marginLeft: 'auto'
-  //   }
-  // }));
-
   const dispatch = useDispatch();
   const history = useHistory();
   const dreams = useSelector(store => store.allDreamsReducer);
-  const user = useSelector((store) => store.user);
+  const user = useSelector(store => store.user);
 
   useEffect(() => {
     dispatch({ type: 'GET_ALL_DREAMS' });
@@ -36,73 +23,60 @@ function InfoPage() {
     <main>
       <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
 
+
         <h1>Dream List</h1>
 
+        Grid for
         <Box>
-          <Grid
-            container
-            xs={12}
-            spacing={3}
-            columnGap={4}
-            rowGap={5}>
-            {dreams.map((dream) =>
+          <Grid container xs={12} spacing={3} columnGap={4} rowGap={5}>
+            {dreams.map(dream => (
 
-              <div className="flippable-card">
-
+              <div className="flippable-card" key={dream.id}>
                 <div className="card">
-                  <Card className="card-front" sx={{ width: 300, position: 'relative' }} key={dream.id}>
-
+                  <Card className="card-front" sx={{ width: 300 }}>
+                    {/* Front content */}
                     <CardMedia
                       sx={{ height: 256 }}
                       image={dream.dream_image_url}
                       title={dream.title}
                       onClick={() => history.push(`/details/${dream.id}`)}
                     />
-
                     <CardContent sx={{ height: 100 }}>
                       <Typography gutterBottom variant="h5" component="div">
                         {dream.title}
                       </Typography>
-
                       <Typography variant="body2" color="text.secondary">
                         {dream.dream_description}
                       </Typography>
                     </CardContent>
                   </Card>
-                </div>
 
-                <div className="card">
-                  <Card className="card-back" sx={{ width: 300, position: 'relative' }} key={dream.id}>
-
+                  <Card className="card-back" sx={{ width: 300 }}>
+                    {/* Back content */}
                     <CardMedia
                       sx={{ height: 256 }}
                       image={dream.dream_image_url}
                       title={dream.title}
                       onClick={() => history.push(`/details/${dream.id}`)}
                     />
-
                     <CardContent sx={{ height: 100 }}>
                       <Typography gutterBottom variant="h5" component="div">
                         {dream.title}
                       </Typography>
-
                       <Typography variant="body2" color="text.secondary">
                         {dream.dream_description}
                       </Typography>
                     </CardContent>
                   </Card>
                 </div>
-
-
-
               </div>
-            )}
+
+            ))}
           </Grid>
         </Box>
 
 
-        <Box
-          sx={{ display: 'flex' }}>
+        <Box sx={{ display: 'flex' }}>
           <div>
             <Card sx={{ width: 400 }}>
               <h3>Welcome, {user.username}!</h3>
@@ -112,9 +86,10 @@ function InfoPage() {
           </div>
         </Box>
 
+
       </Box>
-    </main >
-  )
+    </main>
+  );
 }
 
 export default InfoPage;
