228f4378840371a050b9ec419632a2701d3ede90
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-09-10 20:20:17 -0500

    added put and delete routes, need to implement frontent code for them

--- server/routes/dream.router.js
+++ server/routes/dream.router.js
@@ -4,6 +4,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
+
     if (req.isAuthenticated()) {
         const params = req.user.id;
         const query = `SELECT * FROM "dream" WHERE "user_id" = $1 ORDER BY "date" DESC `;
@@ -29,13 +30,51 @@ router.post('/', (req, res) => {
         pool
             .query(queryText, [user_id, dream_description])
             .then(() => res.sendStatus(201))
-            .catch((err) => {
-                console.log('New dream upload failed: ', err);
+            .catch((error) => {
+                console.log('New dream upload failed: ', error);
                 res.sendStatus(500);
             });
     }
 });
 
 
+router.delete('/', (req, res) => {
 
+    if (req.isAuthenticated()) {
+        pool
+            .query(`DELETE FROM "dream" WHERE id=$1`, [req.params.id])
+            .then((result) => {
+                res.sendStatus(200);
+            }).catch((error) => {
+                console.log('Error DELETE /api/dream', error);
+                res.sendStatus(500);
+            })
+    }
+});
+
+
+router.put('/', (req, res) => {
+
+    if (req.isAuthenticated()) {
+
+        let idToUpdate = req.params.id
+        let NEWdream_description = req.body.dream_description
+        let sqlValues = [NEWdream_description, idToUpdate]
+
+        let sqlQuery = `
+        UPDATE "dream"
+        SET "dream_description" = $1
+        WHERE "id" = $2;
+         `;
+
+        pool.query(sqlQuery, sqlValues)
+            .then((res) => {
+                console.log('successful update for PUT /api/dream', res);
+                res.sendStatus(201)
+            }).catch((error) => {
+                console.error('Error PUT /api/dream', error);
+                res.sendStatus(500)
+            })
+    }
+});
 module.exports = router;
