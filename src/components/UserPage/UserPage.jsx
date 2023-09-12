83e9d53788053e4caa30b10c5cca5219636581e7
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-09-11 21:10:24 -0500

    layout for user homepage edited

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -3,6 +3,13 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useState } from 'react';
 import { useHistory } from 'react-router-dom'
 
+// Importing in MUI
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Grid from '@mui/material/Unstable_Grid2';
+import { Button } from '@mui/material';
+
+
 
 function UserPage() {
 
@@ -25,22 +32,52 @@ function UserPage() {
     history.push('/info')
   }
 
+  const buttonStyle = {
+    backgroundColor: 'black',
+    color: 'white',
+    '&:hover': {
+      backgroundColor: 'darkgray',
+    }
+  };
+
 
   return (
-    <div className="container">
+    <Box>
 
-      <h2>Describe your dream:</h2>
-      <form>
-        <textarea type="text"
-          name="Dream_Description"
-          placeholder='Enter dream description here...'
-          className="form-control"
-          value={dream_description}
-          onChange={(event) => setDream_Description(event.target.value)}>
-        </textarea>
-        <button onClick={handleSubmit} className='btn'>Save Dream!</button>
-      </form>
-    </div >
+      <Grid container
+        rowGap={5}
+        columnGap={5}
+        alignItems="center"
+        justifyContent="center"
+        xs={12}>
+
+
+        <Grid>
+
+          <Box>
+            <h2>Describe your dream:</h2>
+          </Box>
+
+          <Box>
+            <form>
+              <textarea type="text"
+                name="Dream_Description"
+                placeholder='Enter dream description here...'
+                className="form-control"
+                style={{ height: '20em', width: '30em' }}
+                value={dream_description}
+                onChange={(event) => setDream_Description(event.target.value)}>
+              </textarea>
+            </form>
+          </Box>
+
+          <Button onClick={handleSubmit} style={buttonStyle}>Save Dream!</Button>
+
+        </Grid>
+
+
+      </Grid>
+    </Box >
   );
 }
 
